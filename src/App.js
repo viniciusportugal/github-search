3a749f64544a4cc9d59311b47bfd78e3d29c7275
viniciusportugal <vini.aport@gmail.com>
2020-08-21 18:07:27 -0300

    creating application logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.scss';
 
-import Button from './components/Button'
+import Logo from './components/Logo'
 import SearchBar from './components/SearchBar'
+import Button from './components/Button'
 
 function App() {
   return (
     <div className="App">
+      <div className="App__logo">
+        <Logo />
+      </div>
       <div className="App__search">
         <SearchBar placeholder="Digite o nome do usuário" />
         <Button text="Buscar" />
---
+++ src/components/Logo/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './_index.scss'
+
+import GithubLogo from '../../assets/images/github.png'
+
+const Logo = (props) => (
+    <img src={GithubLogo} alt="logo do Github" />
+);
+
+export default Logo;
