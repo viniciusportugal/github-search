990578fa220bed21ac9fea3519ede006c4b882f8
viniciusportugal <vini.aport@gmail.com>
2020-08-22 00:51:11 -0300

    fixing security warning for the direct link of the repository

--- src/components/Repository/index.js
+++ src/components/Repository/index.js
@@ -5,9 +5,10 @@ import './_index.scss'
 const Repository = (props) => (
     <div className="Repository">
         <div className="Repository__name">
-            <a href={props.url} target="_blank">
+            <a href={props.url} target="_blank" rel="noopener noreferrer">
                 <p>{props.name}</p>
             </a>
+            <p className="Repository__description">{props.description}</p>
         </div>
         <div className="Repository__stars">
             <img src={StarIcon} alt="ícone de estrela" className="Repository__stars__icon" />
