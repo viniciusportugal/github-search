38436564e7d35cfde56a4ff5f90cd180e17d9741
viniciusportugal <vini.aport@gmail.com>
2020-08-23 16:00:47 -0300

    creating test ids for avatar and adding more tests to verify if the Avatar props are rendering correctly

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -5,13 +5,15 @@ const Avatar = (props) => (
     <div className="Avatar">
         <img
             src={props.src}
-            alt="Logo do Github"
+            alt="imagem"
+            data-testid="avatar-image"
             className={`Avatar__img img--${props.rounded ? 'rounded' : ''}`}
         />
         {
             props.text ? (
                 <p
                     className={`Avatar__text text--${props.textSize} text--${props.textColor}`}
+                    data-testid="avatar-text"
                 >
                     {props.text}
                 </p>
--- src/components/Avatar/index.test.js
+++ src/components/Avatar/index.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Avatar from './Avatar';
+import { render } from '@testing-library/react';
+import Avatar from './index';
 
-it('renders without crashing', () => {
+import GithubLogo from '../../assets/images/github.png';
+
+it('renders Avatar component without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Avatar />, div);
+});
+
+test('test if prop text is rendering in Avatar Component', () => {
+    const { getByTestId } = render(<Avatar text="vinicius almeida"/>);
+    const linkElement = getByTestId('avatar-text');
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('test if image is rendering in Avatar Component', () => {
+    const { getByTestId } = render(<Avatar src={GithubLogo}/>);
+    const linkElement = getByTestId('avatar-image');
+    expect(linkElement).toBeInTheDocument();
 });
